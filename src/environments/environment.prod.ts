777067a137bdd7f2de17c549aabba61d5be3bb1d
ahmed <ahmedmostafa05031991@gmail.com>
2019-05-12 16:38:08 +0200

    add basic structure

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCMnUxOBh8l7Aex1k9ynpWOJVd5bGlOwzc',
+    authDomain: 'organic-shop-73dca.firebaseapp.com',
+    databaseURL: 'https://organic-shop-73dca.firebaseio.com',
+    projectId: 'organic-shop-73dca',
+    storageBucket: 'organic-shop-73dca.appspot.com',
+    messagingSenderId: '627954610377',
+    appId: '1:627954610377:web:ef8ea3bc920f6cd9'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyCMnUxOBh8l7Aex1k9ynpWOJVd5bGlOwzc',
+    authDomain: 'organic-shop-73dca.firebaseapp.com',
+    databaseURL: 'https://organic-shop-73dca.firebaseio.com',
+    projectId: 'organic-shop-73dca',
+    storageBucket: 'organic-shop-73dca.appspot.com',
+    messagingSenderId: '627954610377',
+    appId: '1:627954610377:web:ef8ea3bc920f6cd9'
+  }
 };
 
 /*
