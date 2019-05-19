8e974f504d5d3acc6eb295085138e805e3a1a5e6
ahmed <ahmedmostafa05031991@gmail.com>
2019-05-19 02:23:31 +0200

    add redirect after auth check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'organic-shop';
+
+  constructor(private auth: AuthService, router: Router) {
+    auth.user$.subscribe(user => {
+      if (user) {
+        const returnUrl = localStorage.getItem('returnUrl');
+        router.navigateByUrl(returnUrl);
+      }
+    });
+  }
 }
--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { AuthService } from './auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterStateSnapshot } from '@angular/router';
 import { CanActivate } from '@angular/router';
 import { map } from 'rxjs/operators';
 
@@ -11,13 +11,13 @@ export class AuthGurdService implements CanActivate {
 
   constructor(private auth: AuthService, private router: Router) {}
 
-  canActivate() {
+  canActivate(route, state: RouterStateSnapshot) {
     return this.auth.user$.pipe(map(user => {
       if (user) {
         return true;
       }
 
-      this.router.navigate(['/login']);
+      this.router.navigate(['/login', {queryPrams: {returnUrl: state.url}}]);
       return false;
     }));
   }
